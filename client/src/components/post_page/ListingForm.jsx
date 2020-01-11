18b54aaa33f7ab885717075cd0cfd64b708177fa
Mark Fuechec <fuechec.mark@gmail.com>
2020-01-11 16:51:48 -0600

    Started Bootstrap form

---
+++ client/src/components/post_page/ListingForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default class ListingForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        // const [validated, setValidated] = useState(false);
+    }
+    
+    handleSubmit(event) {
+        console.log('get money')
+    }
+
+    render() {
+        return(
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Row>
+                    <Form.Group md="4" controlId="validationCustom01">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Name"
+                            defaultValue=""
+                        />
+                    </Form.Group>
+                    <Form.Group md="4" controlId="validationCustom01">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            required
+                            type="email"
+                            placeholder="Email"
+                            defaultValue=""
+                        />
+                    </Form.Group>
+                    <Form.Group md="4" controlId="validationCustom01">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control
+                            required
+                            type="phone"
+                            placeholder="Phone"
+                            defaultValue=""
+                        />
+                    </Form.Group>
+                </Form.Row>
+                <Button type="submit">Submit form</Button>
+            </Form>
+        )
+    }
+}
