cfa5bd70ca1a9377a4b8b54403383718ff76c0b2
Natalia Malesa <nmalesa@gmail.com>
2020-01-12 15:25:30 -0600

    Expand contact form to include name, e-mail, phone, and message area

--- client/src/components/listing_page/ListingContactInfo.jsx
+++ client/src/components/listing_page/ListingContactInfo.jsx
@@ -13,13 +13,30 @@ class ListingContactInfo extends React.Component {
     let listing = dummyData.test.data;
     let userName = listing.userInfo.name;
     let userEmail = listing.userInfo.email;
+    let userPhone = listing.userInfo.phone;
+    let helpfulDetails = "Helpful details may include..."
 
     return (
       <Form>
         <h4>Send {userName} a message:</h4>
         <Form.Group>
-          <Form.Label>E-mail:</Form.Label>
-          <Form.Control type="email" placeholder={userEmail} />
+          <Form.Label>Enter your name:</Form.Label>
+          <Form.Control type="text" placeholder="Example:  Ben Shaw" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your e-mail address:</Form.Label>
+          <Form.Control type="email" placeholder="Example:  bshaw14@gmail.com" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your phone number:</Form.Label>
+          <Form.Control type="text" placeholder="Example: (555) 456-1234" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Select your dates:</Form.Label>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your message:</Form.Label>
+          <Form.Control as="textarea" rows="3" placeholder={helpfulDetails}/>
         </Form.Group>
       </Form>
     );
