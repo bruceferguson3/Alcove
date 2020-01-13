fe1276b3781be5481a9b662061e122b0403badab
Natalia Malesa <nmalesa@gmail.com>
2020-01-13 15:58:39 -0600

    Refactor maps components to accept props.
    Co-authored by Tim Sanderson <twsand11@gmail.com>

--- client/src/components/listing_page/ListingMap.jsx
+++ client/src/components/listing_page/ListingMap.jsx
@@ -9,6 +9,8 @@ const ListingMap = props => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <WrappedMap
+        lat={30.267153}
+        lng={-97.743057}
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
         loadingElement={<div style={{height: "100%"}} />}
         containerElement={<div style={{height: "100%"}} />}
@@ -19,3 +21,5 @@ const ListingMap = props => {
 };
 
 export default ListingMap;
+
+// defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
--- client/src/components/listing_page/WrappedMap.jsx
+++ client/src/components/listing_page/WrappedMap.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap, Circle } from 'react-google-maps';
 
-function Map() {
-  return (
+const WrappedMap = withScriptjs(withGoogleMap((props) => 
     <GoogleMap
       defaultZoom={16}
-      defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
+      defaultCenter={{ lat: props.lat, lng: props.lng }}
     >
       <Circle
-        defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
+        defaultCenter={{ lat: props.lat, lng: props.lng }}
         radius={300}
         options={{ strokeColor: "#194553" }}
       />
     </GoogleMap>
-  );
-};
+))
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
 
+// defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
 
 
 export default WrappedMap;
