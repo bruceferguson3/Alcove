da6afe749c27dd4e302e8ad6f069c16d27674597
Ish Tahir <tahirismaeel@gmail.com>
2020-01-14 11:39:37 -0600

    Removed jumbotron

--- client/src/components/landing_page/LandingPage.jsx
+++ client/src/components/landing_page/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './LandingPage.css';
@@ -7,21 +6,24 @@ import './LandingPage.css';
 const LandingPage = props => (
   <>
     <section className="it-section-1">
-      <Jumbotron className="it-jt">
+      <div className="it-container">
         <div className="it-cta-text">
           <h1>Alcove is here for all your storage needs!</h1>
           <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         </div>
         <div className="it-cta-form">
-          <p>
-            <Form.Control type="text" placeholder="Enter zip code" className="it-search-input" />
-          </p>
-          <p>
-            <Button variant="primary">Find Storage Units</Button>
-          </p>
+          <Form.Control type="text" placeholder="Enter zip code" className="it-search-input" />
+          <button variant="outline-primary" className="btn btn-outline-primary it-btn">
+            Find Storage Units
+          </button>
+          <button variant="outline-secondary" className="btn btn-outline-secondary it-btn">
+            Post The Shits
+          </button>
         </div>
+      </div>
+      <div className="it-image">
         <img className="it-cta-image" src="http://zooki.react.themesbrand.com/images/home-2-img.png" alt="image" />
-      </Jumbotron>
+      </div>
     </section>
     <section className="it-section-2"></section>
     <section className="it-section-3"></section>
