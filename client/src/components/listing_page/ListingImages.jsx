2916278084bc474e64e1308ad02fa856493876b1
Tim Sanderson <twsand11@gmail.com>
2020-01-14 11:04:29 -0600

    Modal function skeleton

--- client/src/components/listing_page/ListingImages.jsx
+++ client/src/components/listing_page/ListingImages.jsx
@@ -1,79 +1,89 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
+import ListingModal from "./ListingModal.jsx";
 
 import dummyData from "./dummyData.js";
 
 const ListingImages = props => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   let images = dummyData.test.data.thumbs;
 
-  if (images.length === 1) {
-    return (
-      <Container>
-        <Row>
-          <Col md={3}></Col>
-          <Col md={6}>
-            <img src={images[0]} />
-          </Col>
-          <Col md={3}></Col>
-        </Row>
-      </Container>
-    );
-  }
+  // if (images.length === 1) {
+  //   return (
+  //     <Container>
+  //       <Row>
+  //         <Col md={3}></Col>
+  //         <Col md={6}>
+  //           <img src={images[0]} />
+  //         </Col>
+  //         <Col md={3}></Col>
+  //       </Row>
+  //     </Container>
+  //   );
+  // }
 
-  if (images.length === 2) {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <img src={images[0]} />
-          </Col>
-          <Col>
-            <img src={images[1]} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  // if (images.length === 2) {
+  //   return (
+  //     <Container>
+  //       <Row>
+  //         <Col>
+  //           <img src={images[0]} />
+  //         </Col>
+  //         <Col>
+  //           <img src={images[1]} />
+  //         </Col>
+  //       </Row>
+  //     </Container>
+  //   );
+  // }
 
-  if (images.length === 3 || images.length === 4) {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <img src={images[0]} />
-          </Col>
-          <Col>
-            <img src={images[1]} />
-          </Col>
-          <Col>
-            <img src={images[2]} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  // if (images.length === 3 || images.length === 4) {
+  //   return (
+  //     <Container>
+  //       <Row>
+  //         <Col>
+  //           <img src={images[0]} />
+  //         </Col>
+  //         <Col>
+  //           <img src={images[1]} />
+  //         </Col>
+  //         <Col>
+  //           <img src={images[2]} />
+  //         </Col>
+  //       </Row>
+  //     </Container>
+  //   );
+  // }
 
-  if (images.length >= 5) {
-    return (
-      <Container>
-        <Row>
-          <Col md={6}>{images[0]}</Col>
-          <Col md={6}>
-            <Row>
-              <Col md={6}>{images[1]}</Col>
-              <Col md={6}>{images[2]}</Col>
-            </Row>
-            <Row>
-              <Col md={6}>{images[3]}</Col>
-              <Col md={6}>{images[4]}</Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  // if (images.length >= 5) {
+  //   return (
+  //     <Container>
+  //       <Row>
+  //         <Col md={6}>{images[0]}</Col>
+  //         <Col md={6}>
+  //           <Row>
+  //             <Col md={6}>{images[1]}</Col>
+  //             <Col md={6}>{images[2]}</Col>
+  //           </Row>
+  //           <Row>
+  //             <Col md={6}>{images[3]}</Col>
+  //             <Col md={6}>{images[4]}</Col>
+  //           </Row>
+  //         </Col>
+  //       </Row>
+  //     </Container>
+  //   );
+  // }
 
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <Button onClick={handleShow}>Launch the super cool modal!</Button>
+      <ListingModal show={show} handleClose={handleClose} />
+    </div>
+  );
 };
 
 export default ListingImages;
---
+++ client/src/components/listing_page/ListingModal.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Modal, Carousel } from "react-bootstrap";
+
+const ListingModal = ({ show, handleClose }) => {
+  return (
+    <Modal
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={show}
+      onHide={handleClose}
+    >
+      <Carousel>
+        <Carousel.Item>
+          <img src="https://cdn.vox-cdn.com/thumbor/w5UsT_-QSFoXLKjGjNxViIzOSt0=/0x0:900x1200/1200x0/filters:focal(0x0:900x1200):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/12486807/Clown_3.0.jpg" />
+          <Carousel.Caption>
+            <p>Creepy clown, probably eats horses or something</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="https://r.hswstatic.com/w_1024/gif/how-clowns-work-orig.jpg" />
+          <Carousel.Caption>
+            <p>Pic 2</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Modal.Dialog>
+        <Modal.Body>
+          <p>Hello Natalia</p>
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal>
+  );
+};
+
+export default ListingModal;
