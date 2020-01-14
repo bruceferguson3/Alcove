3bc975886da5974eddcb89e929b6c466dbff9fa1
Natalia Malesa <nmalesa@gmail.com>
2020-01-14 11:07:18 -0600

    Add FontAwesome libraries as devDependencies

--- client/src/components/listing_page/ListingImages.jsx
+++ client/src/components/listing_page/ListingImages.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Modal, Carousel, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import dummyData from "./dummyData.js";
 
