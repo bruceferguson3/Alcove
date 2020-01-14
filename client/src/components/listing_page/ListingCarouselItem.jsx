680073e61cd57d3a60214f8e486a0cc6b6c0afa8
Tim Sanderson <twsand11@gmail.com>
2020-01-14 12:23:03 -0600

    Create image modal and carousel for listing page
    Co-authored by Natalia Malesa <nmalesa@gmail.com>

--- client/src/components/listing_page/ListingCarouselItem.jsx
+++ client/src/components/listing_page/ListingCarouselItem.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 
 const ListingCarouselItem = ({ src, caption }) => {
-  console.log(src);
   return (
     <Carousel.Item>
       <img className="tsTestPic" src={src} />
