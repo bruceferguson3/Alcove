0f25a107c8378112aef1b2e4843d8e293691c3ed
Ish Tahir <tahirismaeel@gmail.com>
2020-01-15 15:19:40 -0600

    Added header container

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,41 +9,43 @@ import './Header.css';
 
 const Header = props => {
   return (
-    <header className="it-header">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#">
-          <div className="logo-container">
-            <img className="it-logo" src="../../assets/alcove.jpg" alt="Alcove Logo" />
-            <span className="logo-text">Alcove</span>
-          </div>
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          <IndexLinkContainer to="/">
-            <Nav.Link onClick={() => props.changePath('/')}>Home</Nav.Link>
-          </IndexLinkContainer>
-          <LinkContainer to="/about">
-            <Nav.Link onClick={() => props.changePath('/about')}>About</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/features">
-            <Nav.Link onClick={() => props.changePath('/features')}>Features</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-            <Nav.Link onClick={() => props.changePath('/contact')}>Contact Us</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        {props.path !== '/' ? (
-          <Form inline>
-            <FormControl type="text" placeholder="Enter zip code" className="mr-sm-2 it-search" />
-            <Button variant="primary" className="it-btn-header it-btn-primary">
-              Find Items/Units
-            </Button>
-            <Button variant="secondary" className="it-btn-header it-btn-secondary">
-              Post Items/Units
-            </Button>
-          </Form>
-        ) : null}
-      </Navbar>
-    </header>
+    <div className="it-header-container">
+      <header className="it-header">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#">
+            <div className="logo-container">
+              <img className="it-logo" src="../../assets/alcove.jpg" alt="Alcove Logo" />
+              <span className="logo-text">Alcove</span>
+            </div>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <IndexLinkContainer to="/">
+              <Nav.Link onClick={() => props.changePath('/')}>Home</Nav.Link>
+            </IndexLinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link onClick={() => props.changePath('/about')}>About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/features">
+              <Nav.Link onClick={() => props.changePath('/features')}>Features</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link onClick={() => props.changePath('/contact')}>Contact Us</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          {props.path !== '/' ? (
+            <Form inline>
+              <FormControl type="text" placeholder="Enter zip code" className="mr-sm-2 it-search" />
+              <Button variant="primary" className="it-btn-header it-btn-primary">
+                Find Items/Units
+              </Button>
+              <Button variant="secondary" className="it-btn-header it-btn-secondary">
+                Post Items/Units
+              </Button>
+            </Form>
+          ) : null}
+        </Navbar>
+      </header>
+    </div>
   );
 };
 
