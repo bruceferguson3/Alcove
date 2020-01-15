ebd36e6d6570cbc14ee6bfc2d8192c22378220f3
Ish Tahir <tahirismaeel@gmail.com>
2020-01-15 12:19:44 -0600

    Added link containers for header links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,21 +4,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 import './Header.css';
 
 const Header = props => {
   return (
     <header className="it-header">
       <Navbar bg="dark" variant="dark">
-        <div className="logo-container">
-          <img className="it-logo" src="../../assets/alcove.jpg" alt="Alcove Logo" />
-          <span className="logo-text">Alcove</span>
-        </div>
+        <Navbar.Brand href="#">
+          <div className="logo-container">
+            <img className="it-logo" src="../../assets/alcove.jpg" alt="Alcove Logo" />
+            <span className="logo-text">Alcove</span>
+          </div>
+        </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">About</Nav.Link>
-          <Nav.Link href="#features">Reviews</Nav.Link>
-          <Nav.Link href="#pricing">Contact Us</Nav.Link>
+          <IndexLinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </IndexLinkContainer>
+          <LinkContainer to="/about">
+            <Nav.Link>About</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/features">
+            <Nav.Link>Features</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/contact">
+            <Nav.Link>Contact Us</Nav.Link>
+          </LinkContainer>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Enter zip code" className="mr-sm-2" />
