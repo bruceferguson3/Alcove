560f451fb5f2e04aa01714a3e1a63c8a0caaaa58
Natalia Malesa <nmalesa@gmail.com>
2020-01-15 12:18:59 -0600

    Create preview modal that successfully renders to the page

--- client/src/components/preview_page/PreviewPage.jsx
+++ client/src/components/preview_page/PreviewPage.jsx
@@ -4,33 +4,38 @@ import { Button, Modal } from 'react-bootstrap';
 import dummyData from './dummyData.js';
 
 const PreviewPage = props => {
-  // const [show, setShow] = useState(false);
-  //
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-  //
-  // return (
-  //   <div>
-  //     <Button onClick={handleShow}>Test the preview page.</Button>
-  //     <Modal
-  //       size="lg"
-  //       aria-labelledby="contained-modal-title-vcenter"
-  //       centered
-  //       show={show}
-  //       handleClose={handleClose}
-  //     >
-  //       <Modal.Dialog>
-  //         <Modal.Body>
-  //           <p>Test the modal.</p>
-  //         </Modal.Body>
-  //       </Modal.Dialog>
-  //     </Modal>
-  //   </div>
-    return (
-      <div>
-        <p>Hello World!</p>
-      </div>
-    )
-}
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button onClick={handleShow}>Test the preview page.</Button>
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={show}
+        handleClose={handleClose}
+      >
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Preview of Your Post</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>Test the modal.</p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save and Submit</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </Modal>
+    </div>
+  );
+};
 
 export default PreviewPage;
