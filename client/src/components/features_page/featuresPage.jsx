39e82dc111cc686d6575a21ef9006a924b15ad54
JCDiaz1201 <jcdiaz1201@gmail.com>
2020-01-17 12:00:06 -0600

    Feature page done

--- client/src/components/features_page/featuresPage.jsx
+++ client/src/components/features_page/featuresPage.jsx
@@ -42,6 +42,20 @@ const Features = props => {
         </div>
         <img src="../../assets/connect.jpg" className="jd-sub-img-three"></img>
       </div>
+
+      <div className="jd-sub-wrapper">
+        <div className="jd-sub-four">
+          <h1>Every listing is verified and vetted for your peace of mind</h1>
+          <p>
+            Praesent auctor quis lorem sit amet varius. Sed vulputate odio vitae justo scelerisque congue.
+            Integer suscipit quam sed nunc accumsan iaculis.
+            Duis auctor neque justo, quis semper enim hendrerit vehicula.
+            Praesent auctor quis lorem sit amet varius. Sed vulputate odio vitae justo scelerisque congue.
+            Integer suscipit quam sed nunc accumsan iaculis.
+        </p>
+        </div>
+        <img src="../../assets/happy.jpg" className="jd-sub-img-four"></img>
+      </div>
     </div>
   );
 };
