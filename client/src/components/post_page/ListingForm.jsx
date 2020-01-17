9ebc1ffd905b502a9b032769f2cbb4f9bf77d6f4
Mark Fuechec <fuechec.mark@gmail.com>
2020-01-17 16:12:21 -0600

    Formatted post page step 1 for 13 inch screens

--- client/src/components/post_page/ListingForm.jsx
+++ client/src/components/post_page/ListingForm.jsx
@@ -269,7 +269,7 @@ export default class ListingForm extends React.Component {
         if (this.state.cardCounter === 0) {
             return (
                 <div className='mycustom-jumbotron jumbotron container col mb-0'>
-                    <h1 className="display-4 mb-3">What are your storage needs?</h1>
+                    <h1 id="postTitle1" className="display-4 mb-3">What are your storage needs?</h1>
                     <ProgressBar animated now={0} />
                     <div className='postFormContainer col shadow-lg p-3'>
                         <Step1 recordStateInfo={this.recordStateInfo} nextButton={this.nextButton} />
@@ -279,7 +279,7 @@ export default class ListingForm extends React.Component {
         } else if (this.state.cardCounter === 1) {
             return (
                 <div className='mycustom-jumbotron jumbotron container col mb-0'>
-                    <h1 className="display-4 mb-3">Contact Information</h1>
+                    <h1 id="postTitle2" className="display-4 mb-3">Contact Information</h1>
                     <ProgressBar animated now={25} />
                     <div className='postFormContainer col shadow-lg p-3'>
                         <Step2 getCoords={this.getCoords} nextButton={this.nextButton} backButton={this.backButton} recordStateInfo={this.recordStateInfo}
@@ -292,7 +292,7 @@ export default class ListingForm extends React.Component {
             )
         } else if (this.state.cardCounter === 2) {
             return (
-                <div className='mycustom-jumbotron jumbotron container col mb-0'>
+                <div id="postTitle3" className='mycustom-jumbotron jumbotron container col mb-0'>
                     <h1 className="display-4 mb-3">Features</h1>
                     <ProgressBar animated now={50} />
                     <div className='postFormContainer col shadow-lg p-3'>
@@ -302,7 +302,7 @@ export default class ListingForm extends React.Component {
             )
         } else if (this.state.cardCounter === 3) {
             return (
-                <div className='mycustom-jumbotron jumbotron container col mb-0'>
+                <div id="postTitle4" className='mycustom-jumbotron jumbotron container col mb-0'>
                     <h1 className="display-4 mb-3">Posting Description</h1>
                     <ProgressBar animated now={75} />
                     <div className='postFormContainer col shadow-lg p-3'>
