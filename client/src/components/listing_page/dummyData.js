c5d03bef4da4b42864a97a257ebd8837a7e88a91
Tim Sanderson <twsand11@gmail.com>
2020-01-18 10:51:02 -0600

    Fixed borders and added box shadow

--- client/src/components/listing_page/dummyData.js
+++ client/src/components/listing_page/dummyData.js
@@ -30,9 +30,9 @@ let test = {
     thumbs: [
       "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_outside.jpg",
       "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_inside.jpg",
-      // "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_snow.jpg",
-      // "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_snow.jpg",
-      // "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_inside.jpg"
+      "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_snow.jpg",
+      "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_snow.jpg",
+      "https://alcove-image-size-four.s3.us-east-2.amazonaws.com/barn1_inside.jpg"
     ],
     title: "Spacious Area",
     reviews: ["", "", ""],
