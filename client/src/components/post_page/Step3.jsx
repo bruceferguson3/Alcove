ec190ca7899c3992ce1915a5a222159e3eaa5877
Tim Sanderson <twsand11@gmail.com>
2020-03-01 13:56:43 -0600

    Changed tooltipText to render with one function call that returns an object

--- client/src/components/post_page/Step3.jsx
+++ client/src/components/post_page/Step3.jsx
@@ -21,14 +21,15 @@ const Step3 = ({
   invalidStepThree,
   backButton
 }) => {
-  let sizeTooltipText = assignText(type, "size");
-  let frequencyTooltipText = assignText(type, "frequency");
-  let durationTooltipText = assignText(type, "duration");
-  let lockedPrompt = assignText(type, "locked");
-  let exclusivePrompt = assignText(type, "exclusive");
-  let indoorsPrompt = assignText(type, "indoors");
-  let climateControlPromptPositive = assignText(type, "climateControlPositive");
-  let climateControlPromptNegative = assignText(type, "climateControlNegative");
+  let tooltipText = assignText(type);
+  // let sizeTooltipText = assignText(type, "size");
+  // let frequencyTooltipText = assignText(type, "frequency");
+  // let durationTooltipText = assignText(type, "duration");
+  // let lockedPrompt = assignText(type, "locked");
+  // let exclusivePrompt = assignText(type, "exclusive");
+  // let indoorsPrompt = assignText(type, "indoors");
+  // let climateControlPromptPositive = assignText(type, "climateControlPositive");
+  // let climateControlPromptNegative = assignText(type, "climateControlNegative");
 
   let climateControlBoolean = indoors ? {} : { visibility: "hidden" };
 
@@ -37,9 +38,9 @@ const Step3 = ({
   return (
     <div>
       <div className="tsDropdownContainer">
-        <StepThreeButtons type="size" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={sizeTooltipText} typeValue={size} />
-        <StepThreeButtons type="frequency" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={frequencyTooltipText} typeValue={easeOfAccess} />
-        <StepThreeButtons type="duration" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={durationTooltipText} typeValue={duration} />
+        <StepThreeButtons type="size" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={tooltipText.size} typeValue={size} />
+        <StepThreeButtons type="frequency" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={tooltipText.frequency} typeValue={easeOfAccess} />
+        <StepThreeButtons type="duration" formError={formError} recordFilterInfo={recordFilterInfo} tooltipText={tooltipText.duration} typeValue={duration} />
       </div>
       <div className="tsFilterCheckboxContainer">
         <ul style={{ marginBottom: '0px' }}>
@@ -53,7 +54,7 @@ const Step3 = ({
                 recordStateInfo(e, 'filters', 'locked', 'defaultCheck6')
               }
             />
-            <label className="form-check-label">{lockedPrompt}</label>
+            <label className="form-check-label">{toooltipText.locked}</label>
           </div>
           <div className="form-check">
             <input
@@ -65,7 +66,7 @@ const Step3 = ({
                 recordStateInfo(e, 'filters', 'standAlone', 'defaultCheck7')
               }
             />
-            <label className="form-check-label">{exclusivePrompt}</label>
+            <label className="form-check-label">{tooltipText.exclusive}</label>
           </div>
           <div style={{ height: '2rem' }} className="form-check">
             <input
@@ -76,7 +77,7 @@ const Step3 = ({
               checked={indoors}
               onChange={(e) => recordFilterInfo('Indoors', e.target.value)}
             />
-            <label className="form-check-label">{indoorsPrompt}</label>
+            <label className="form-check-label">{tooltipText.indoors}</label>
             <div style={climateControlBoolean}>
               <ul id="indoorsList" className="p-1">
                 <div className="custom-control custom-radio">
@@ -93,7 +94,7 @@ const Step3 = ({
                     className="custom-control-label"
                     htmlFor="climateControl"
                   >
-                    {climateControlPromptPositive}
+                    {tooltipText.climateControlPositive}
                   </label>
                 </div>
                 <div className="custom-control custom-radio">
@@ -110,7 +111,7 @@ const Step3 = ({
                     className="custom-control-label"
                     htmlFor="climateControl2"
                   >
-                    {climateControlPromptNegative}
+                    {tooltipText.climateControlNegative}
                   </label>
                 </div>
               </ul>
@@ -139,51 +140,79 @@ export default Step3;
 
 
 //helper function to assign text for tooltips and prompts
-const assignText = (storageType, tooltip) => {
-  switch (storageType) {
-    case "storage":
-      switch (tooltip) {
-        case "size":
-          return "Choose the relative size of your space here. Small denotes an area about the size of a pantry, medium a room, large a garage";
-        case "frequency":
-          return "Choose how often you are willing to let items be picked up from your area";
-        case "duration":
-          return "Choose how long you are willing to rent the space out for";
-        case "locked":
-          return "Is this space locked?";
-        case "exclusive":
-          return "Can multiple items be stored here?";
-        case "indoors":
-          return "Is this area indoors?";
-        case "climateControlNegative":
-          return "Space is not climate controlled";
-        case "climateControlPositive":
-          return "Space is climate controlled";
-        default:
-          return "Invalid Tooltip"
-      }
-    case "item":
-      switch (tooltip) {
-        case "size":
-          return "Choose the relative size of your item here. A small item might be a snowboard, medium a twin bed, large a motorcycle";
-        case "frequency":
-          return "Choose how often you need to pick up your item from the storage space.";
-        case "duration":
-          return "Choose how long you need to store the item for.";
-        case "locked":
-          return "Do you need the area to be locked?";
-        case "exclusive":
-          return "Are you ok with sharing the space with other items?";
-        case "indoors":
-          return "Do you need the area to be indoors?";
-        case "climateControlNegative":
-          return "I do not need the area to be climate controlled";
-        case "climateControlPositive":
-          return "I need the area to be climate controlled";
-        default:
-          return "Invalid Toooltip"
-      }
-    default:
-      return "Invalid storage type";
+const assignText = (storageType) => {
+  if (storageType === 'storage') {
+    return {
+      size: "Choose the relative size of your space here. Small denotes an area about the size of a pantry, medium a room, large a garage",
+      frequency: "Choose how often you are willing to let items be picked up from your area",
+      duration: "Choose how long you are willing to rent the space out for",
+      locked: "Is this space locked?",
+      exclusive: "Can multiple items be stored here?",
+      indoors: "Is this area indoors?",
+      climateControlNegative: "Space is not climate controlled",
+      climateControlPositive: "Space is climate controlled",
+    }
+  } else {
+    return {
+      size: "Choose the relative size of your item here. A small item might be a snowboard, medium a twin bed, large a motorcycle",
+      frequency: "Choose how often you need to pick up your item from the storage space.",
+      duration: "Choose how long you need to store the item for.",
+      locked: "Do you need the area to be locked?",
+      exclusive: "Are you ok with sharing the space with other items?",
+      indoors: "Do you need the area to be indoors?",
+      climateControlNegative: "I do not need the area to be climate controlled",
+      climateControlPositive: "I need the area to be climate controlled",
+    }
   }
 }
+
+
+
+// const assignText = (storageType, tooltip) => {
+//   switch (storageType) {
+//     case "storage":
+//       switch (tooltip) {
+//         case "size":
+//           return "Choose the relative size of your space here. Small denotes an area about the size of a pantry, medium a room, large a garage";
+//         case "frequency":
+//           return "Choose how often you are willing to let items be picked up from your area";
+//         case "duration":
+//           return "Choose how long you are willing to rent the space out for";
+//         case "locked":
+//           return "Is this space locked?";
+//         case "exclusive":
+//           return "Can multiple items be stored here?";
+//         case "indoors":
+//           return "Is this area indoors?";
+//         case "climateControlNegative":
+//           return "Space is not climate controlled";
+//         case "climateControlPositive":
+//           return "Space is climate controlled";
+//         default:
+//           return "Invalid Tooltip"
+//       }
+//     case "item":
+//       switch (tooltip) {
+//         case "size":
+//           return "Choose the relative size of your item here. A small item might be a snowboard, medium a twin bed, large a motorcycle";
+//         case "frequency":
+//           return "Choose how often you need to pick up your item from the storage space.";
+//         case "duration":
+//           return "Choose how long you need to store the item for.";
+//         case "locked":
+//           return "Do you need the area to be locked?";
+//         case "exclusive":
+//           return "Are you ok with sharing the space with other items?";
+//         case "indoors":
+//           return "Do you need the area to be indoors?";
+//         case "climateControlNegative":
+//           return "I do not need the area to be climate controlled";
+//         case "climateControlPositive":
+//           return "I need the area to be climate controlled";
+//         default:
+//           return "Invalid Toooltip"
+//       }
+//     default:
+//       return "Invalid storage type";
+//   }
+// }
